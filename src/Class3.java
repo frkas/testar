56ef3583397f397120fd320288113409f85c6068
Fredrik Kasström <fredk@redpill-linpro.com>
2015-11-30 12:56:52 +0100

    skljf

--- src/Class3.java
+++ src/Class3.java
@@ -3,7 +3,11 @@ import org.omg.Messaging.SyncScopeHelper;
 public class Class3 {
 	
 	private void kkkkk(){
-		System.out.println("Kalle2");
+		System.out.println("Kalle2 ");
 		System.out.println("KALLE3");
+System.out.println("kkkkk");
+aslkdj
+sadg
+
 	}
 }
